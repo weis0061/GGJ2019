b8ecb57252acc40a0e8613a9a7b44b1566f01ce4
Tyson <tysonjfmoyes@gmail.com>
2019-01-25 19:57:16 -0500

    bee flies left and right

--- beeGame/Assets/BEe.cs
+++ beeGame/Assets/BEe.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BEe : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        GetComponent<Rigidbody>().velocity = new Vector3( Input.GetAxis("Horizontal") * Time.deltaTime*100,0,0);
+        Debug.Log(Input.GetAxis("Horizontal"));
 	}
 }
