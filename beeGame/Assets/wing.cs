b97d5df6df79840e3d42309e481cf6550513a3cc
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 10:17:08 -0500

    3d bee's wings now flap

---
+++ beeGame/Assets/wing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wing : MonoBehaviour {
+    Vector3 initialRotation;
+	// Use this for initialization
+	void Start () {
+        initialRotation = transform.localEulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var newTransform = transform.localEulerAngles;
+        newTransform.y= initialRotation.y + Mathf.Sin(Time.time*20)*45;
+        transform.localEulerAngles = newTransform;
+	}
+}
