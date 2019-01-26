8a7ee5c010eb1121b76b08e37c55eddf79b7d17f
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 13:30:54 -0500

    hp counters

---
+++ beeGame/Assets/hpcounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hpcounter : MonoBehaviour {
+    public TextMesh txt;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        txt.text = BeeHp.beeHp[0].ToString();
+        var pos = Camera.main.ViewportToWorldPoint(new Vector3(0.1F, 0.9F, 0));
+        pos.z = transform.position.z;
+        transform.position = pos;
+	}
+}
