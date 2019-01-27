a2db74357717639b43ded290cf4822b196361a78
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 21:02:37 -0500

    dating sim wip

--- beeGame/Assets/DatingSim.cs
+++ beeGame/Assets/DatingSim.cs
@@ -1,18 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class DatingSim : MonoBehaviour {
+public class DatingSim : MonoBehaviour
+{
+    public GameObject[] questions;
     public GameObject[] correctAnswers;
     public GameObject[][] incorrectAnswers;
+    public Transform[] answerLocations;
+    public Transform questionLocation;
+
+    List<GameObject> thisQuestion=new List<GameObject>();
+    GameObject spawnedCorrectAnswer;
+    int questionNum = 0;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Start ()
+    {
+        foreach (var r in correctAnswers.Select(x => x.GetComponent<DatingSimResponse>()))
+        {
+            r.datingSimManager = this;
+        }
+        foreach (var answerList in incorrectAnswers)
+        {
+            foreach (var r in answerList.Select(x => x.GetComponent<DatingSimResponse>()))
+            {
+                r.datingSimManager = this;
+            }
+        }
+        ShowQuestion();
+    }
+
+    public void selectOption(DatingSimResponse r) {
+        //if the correct answer was picked
+        if (spawnedCorrectAnswer==r)
+        {
+            questionNum++;
+            ShowQuestion();
+        }
+        else
+        {
+            mistake();
+        }
+    }
+
+    void ShowQuestion()
+    {
+        //save the right answer
+        spawnedCorrectAnswer= Instantiate(correctAnswers[questionNum]);
+
+        //add all answers to question currently active
+        thisQuestion.Add(spawnedCorrectAnswer);
+        foreach(var a in incorrectAnswers[questionNum])
+        {
+            thisQuestion.Add(Instantiate(a));
+        }
+        //randomize it
+        thisQuestion = thisQuestion.OrderBy(x => Random.value).ToList();
+
+        //add to location on screen
+        for(int i = 0; i < answerLocations.Length; i++)
+        {
+            thisQuestion[i].transform.position = answerLocations[i].position;
+        }
+    }
+
+    void mistake()
+    {
+        //TODO
+    }
 }
---
+++ beeGame/Assets/DatingSimResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DatingSimResponse : MonoBehaviour {
+    public DatingSim datingSimManager;
+
+    void OnMouseDownAsButton()
+    {
+        datingSimManager.selectOption(this);
+    }
+}
