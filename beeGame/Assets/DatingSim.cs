9b71b6cc65339d9493366262fecfaf96701915d9
Tyson <tysonjfmoyes@gmail.com>
2019-01-26 19:39:53 -0500

    dating sim wip and negative hp fix

---
+++ beeGame/Assets/DatingSim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatingSim : MonoBehaviour {
+    public GameObject[] correctAnswers;
+    public GameObject[][] incorrectAnswers;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
