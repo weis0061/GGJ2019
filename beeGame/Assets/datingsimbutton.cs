b87aeed495c8b3139b203d436d0161486c23ed19
Nathan <nathandubord@gmail.com>
2019-01-27 11:55:53 -0500

    dating sim and secret room update

---
+++ beeGame/Assets/datingsimbutton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class datingsimbutton : MonoBehaviour {
+
+    void OnMouseUpAsButton()
+    {
+
+        Application.LoadLevel("datingsim");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
